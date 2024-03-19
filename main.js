688caebc58d177d149d9127ede3be9e739fd1fe2
shewir <160327341+shewir@users.noreply.github.com>
2024-03-20 02:21:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// console.log("hello world");
+// quetion (2):
+// persnol Message:Store a persons name in avariable and print amessage should be simple such as "hello eric" would you like to learn some python today?
+
+// let myFullName = "muhammad sheraz"
+// console.log(`${myFullName} "is a good person"`);
